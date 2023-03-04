877f6c0c53277a22ac1b1dbda0555e2e45ce5690
Dimi Chakarov <dimitar@dchakarov.com>
2023-03-04 00:13:49 +0000

    Initial Commit

---
+++ LetterPlay/LetterPlayApp.swift
@@ -0,0 +1,17 @@
+//
+//  LetterPlayApp.swift
+//  LetterPlay
+//
+//  Created by Dimi Chakarov on 04/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct LetterPlayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
